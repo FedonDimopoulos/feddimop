f687e853baf3b0f94eeed47ddb373ec527b4e5fe
Your Name <you@example.com>
2023-01-23 20:44:35 +0200

    basic form

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,2 +1,8 @@
 class Property < ApplicationRecord
+  belongs_to :type  # this validates presence of Type
+  
+  validates :title, presence: true
+  validates :area, presence: true
+  validates :price, :numericality => {:only_integer => true}
+  # Description is optional therefore not validated
 end
